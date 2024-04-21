76e542913f5aef0011626d8cefe2d3c849354c4b
Muntazir <muntazirmohammad07@gmail.com>
2024-04-21 11:09:51 -0700

    adding welcome message

--- index.ts
+++ index.ts
@@ -2,13 +2,15 @@
 
 import inquirer from "inquirer"
 
-let randomNumber = Math.floor(Math.random() * 6 + 1);
+console.log("Welcome to Muntazir Mohammad - Number guessing Game!")
+
+let randomNumber = Math.floor(Math.random() * 5 + 1);
 
 const answers = await inquirer.prompt([
     {
         name: "userGuessedNumber",
         type: "number",
-        message: "Please guess a number between 1-6: ",
+        message: "Please guess a number between 1-5: ",
     },
 ]);
 
